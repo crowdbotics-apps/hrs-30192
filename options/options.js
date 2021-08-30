0136d8bd32930607fc499a6f4519c326dcd134e7
Crowdbotics <team@crowdbotics.com>
2021-08-30 08:12:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hrs_30192Identifier",
+  url: "https://hrs_30192Identifier.botics.co",
+  api: "https://hrs_30192Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
